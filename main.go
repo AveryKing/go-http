a16fee3f88c59ab140b4680c01c5f098b3e5aff5
Avery King <kavery086@gmail.com>
2022-06-19 20:45:28 -0500

    data received

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello worldz")
+		d, _ := io.ReadAll(r.Body)
+		log.Printf("Data %s\n", d)
 	})
 
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
